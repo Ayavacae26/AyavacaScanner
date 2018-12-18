60b16affda3754004a2c733366c3469f1f470236
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-12-18 12:21:46 -0600

    add some things

--- lookUpTable.java
+++ lookUpTable.java
@@ -1,13 +1,49 @@
 import java.util.HashMap;
 
 
-public class lookUpTable{
+public class lookUpTable extends HashMap<String, TokenType{
 
-
-
-
-
-
-
+		AND,
+		PROGRAM,
+		ARRAY,
+		BEGIN,
+		DIV,
+		DO,
+		ELSE,
+		FUNCTION,
+		IF,
+		INTERGER,
+		DIGIT,
+		MOD,
+		NOT,
+		OF,
+		OR,
+		PROCEDURE,
+		REAL,
+		THEN,
+		VAR,
+		WHILE,
+		
+	//SYMBOLS
+		SEMICOLON,
+		COMMA,
+		COLON,
+		PERIOD,
+		LEFTBRACKET,
+		RIGHTBRACKET,
+		LEFTPARENTHESES,
+		RIGHTPARENTHESES,
+		PLUS,
+		MINUS,
+		EQUALS,
+		LESS_THAN,
+		LESS_THAN_OR_EQUAL,
+		GREATER_THAN,
+		GREATER_THAN_OR_EQUAL,
+		ASTERISK,
+		SLASH,
+		ASSIGN,
+		GUILLEMENTS,
+		
 
 }
