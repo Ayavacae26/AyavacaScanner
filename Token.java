8c8a73842890eabac4891f7f7dca39fff3b75a27
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-12-17 21:51:17 -0600

    created the token constructor

--- Token.java
+++ Token.java
@@ -1,7 +1,16 @@
 // token class
 
 public class Token {
-
+	private String lexeme;  //defining the lexeme as a string 
+	private TokenType tokentype; //defining the type of token
+	
+	// Constructs a Token based on lexeme and tokentype
+	public Token{ 
+	
+	
+	}
+	
+	
 
 
 
