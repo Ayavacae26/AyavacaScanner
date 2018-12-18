68b2994c300887ebe586d2e1e0adea07a8f550c7
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-12-17 19:17:36 -0600

    add some of the keywords to it

---
+++ TokenType.java
@@ -0,0 +1,28 @@
+
+Public enum TokenType {
+
+	//Keywords
+		AND,
+		PROGRAM,
+		ARRAY,
+		BEGIN,
+		DIV,
+		DO,
+		ELSE,
+		FUNCTION,
+		IF,
+		INTERGER,
+		DIGIT,
+		MOD,
+		NOT,
+		OF,
+		OR,
+		P
+
+
+
+
+
+
+
+}
