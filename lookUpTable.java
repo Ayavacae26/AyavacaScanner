e96c204b9595b70124e370c32946dc1c234fd361
Erik Ayavaca-Tirado <erikayavaca@gmail.com>
2018-12-18 21:10:27 -0600

    updated contents

--- lookUpTable.java
+++ lookUpTable.java
@@ -5,25 +5,25 @@ public class lookUpTable extends HashMap<String, TokenType{
 	public lookUpTable(){
 		HashMap<String, TokenType> Map = new HashMap<String, TokenType>();
 			this.put("AND", TokenType.AND);
-			PROGRAM,
-			ARRAY,
-			BEGIN,
-			DIV,
-			DO,
-			ELSE,
-			FUNCTION,
-			IF,
-			INTERGER,
-			DIGIT,
-			MOD,
-			NOT,
-			OF,
-			OR,
-			PROCEDURE,
-			REAL,
-			THEN,
-			VAR,
-			WHILE,
+			this.put("PROGRAM", TokenType.PROGRAM);
+			this.put("ARRAY", TokenType.ARRAY);
+			this.put("BEGIN", TokenType.BEGIN);
+			this.put("DIV", TokenType.DIV);
+			this.put("DO", TokenType.DO);
+			this.put("ELSE", TokenType.ELSE);
+			this.put("FUNCTION", TokenType.FUNCTION);
+			this.put("IF", TokenType.IF);
+			this.put("INTERGER", TokenType.INTERGER);
+			this.put("DIGIT", TokenType.DIGIT);
+			this.put("MOD", TokenType.MOD);
+			this.put("NOT", TokenType.NOT);
+			this.put("OF", TokenType.OF);
+			this.put("OR", TokenType.OR);
+			this.put("PROCEDURE", TokenType.PROCEDURE);
+			this.put("REAL", TokenType.REAL);
+			this.put("THEN", TokenType.THEN);
+			this.put("VAR", TokenType.VAR);
+			this.put("WHILE", TokenType.WHILE);
 		
 			//SYMBOLS
 			SEMICOLON,
